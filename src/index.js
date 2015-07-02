4e22396ee6e59e051a691d71eb8d76f79bd6bc21
dtothefp <dtothefp@gmail.com>
2015-07-01 23:29:58 -0400

    add karma config and tests

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import moduleA, {dopeness} from './lib/module-a';
 console.log('MAIN JS LOADED');
 var {a, b} = dopeness;
 
-moduleA();
+console.log(moduleA());
 console.log('A => %s, B => %s', a, b);
---
+++ test/vanilla-karma-spec.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('a vanilla karma spec', function() {
+  it('should work', function() {
+    expect(1).to.eq(1);
+  });
+});
